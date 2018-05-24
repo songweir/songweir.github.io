0c3bf57534cec46944ce0dfdb8a25daf8ebb3a82
songzp <465254993@qq.com>
2018-05-24 15:12:23 +0800

    ch

--- h5s/video/YNN/src/js/common.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	var qd = window.localStorage.getItem('qd');
-	var $logoWrap = $('.logo-wrap');
-	if (qd === '1') {
-		$logoWrap.html('<img src="./src/image/yingbeier.png" class="yingbeier">')
-	} else if (qd === '2') {
-		$logoWrap.html('<img src="./src/image/brand_zyyy.png" class="zhongyi">')
-	}
-})
